763cfec623d2e9735c9cf9c0353205140168ecf5
victor <ti11.hrpp@alsf.org.br>
2016-08-22 12:11:15 -0300

    Atualizações cadastro de usuario

--- public/js/classes/user.js
+++ public/js/classes/user.js
@@ -1,6 +1,8 @@
-$(function(){
+$(function () {
 
-    var Usuario = function(data){
+
+    //MODEL
+    var Usuario = function (data) {
         this.nome = ko.observable(data.nome);
         this.email = ko.observable(data.email);
         this.email_conf = ko.observable(data.email_conf);
@@ -8,83 +10,80 @@ $(function(){
         this.senha_conf = ko.observable(data.senha_conf);
     };
 
-    Usuario.prototype.criaUsuario = function(){
+    Usuario.prototype.criaUsuario = function () {
         var request = $.ajax({
             type: "POST",
             url: "/usuario/criar",
-            dataType: "json", 
+            dataType: "json",
             data: {
-                nome : this.nome(),
-                email : this.email(),
-                email_conf : this.email_conf(),
-                senha : this.senha(),
-                senha_conf : this.senha_conf()
+                nome: this.nome(),
+                email: this.email(),
+                email_conf: this.email_conf(),
+                senha: this.senha(),
+                senha_conf: this.senha_conf()
 
             }
         });
 
-        request.done(function(response){
+        request.done(function (response) {
             console.log(response);
-            viewModel.expenses.push(new Expense({
-                desc: response.desc,
-                time: response.time
+            viewModel.usuarios.push(new Usuario({
+                nome: response.nome,
+                email: response.email,
+                email_conf: response.email_conf,
+                senha: response.senha,
+                senha_conf: response.senha_conf
             }));
         });
     }
 
-    var UsuariosViewModel = function() {
+    var UsuariosViewModel = function () {
         var self = this;
-        self.expenses = ko.observableArray();
+        self.usuarios = ko.observableArray();
 
-        self.total = ko.computed(function(){
-            var total = 0;
-            var hourly_rate = 100;
-            for(var p=0; p < self.expenses().length; p++){
-                total += parseFloat(self.expenses()[p].time());
-            }
-            return (total*hourly_rate).toFixed(2);
-        })
-
-        self.addExpense = function(){
-            var expense = new Expense({
-                desc: $('#desc').val(),
-                time: $('#time').val()
+        self.addUsuario = function () {
+            var usuario = new Usuario({
+                nome: $('#nome').val(),
+                email: $('#email').val(),
+                email_conf: $('#email_conf').val(),
+                senha: $('#senha').val(),
+                senha_conf: $('#senha_conf').val()
             });
 
-            expense.addToDB();
+            usuario.criaUsuario();
         }
-        
-        var refresh = function() {
-           var request = $.ajax({
-                url: "/api/expenses/",
-                type: "GET",
-                dataType: "json"
-            });
 
-           request.done(function(response){
-            for (var i=0; i < response.length; i++ ){
-                self.expenses.push(new Expense(response[i]));
-            }
-           });
+        //var refresh = function () {
+        //    var request = $.ajax({
+        //        url: "/usuario/refresh/",
+        //        type: "GET",
+        //        dataType: "json"
+        //    });
 
-           request.fail(function( jqXHR, textStatus ) {
-              console.log( "Request failed: " + textStatus );
-            });
+        //    request.done(function (response) {
+        //        for (var i = 0; i < response.length; i++) {
+        //            self.expenses.push(new Expense(response[i]));
+        //        }
+        //    });
 
-       }
-        //refresh immediately to load initial data
-        refresh();
+        //    request.fail(function (jqXHR, textStatus) {
+        //        console.log("Request failed: " + textStatus);
+        //    });
+
+        //}
+        ////refresh immediately to load initial data
+        //refresh();
 
     };
 
 
-	var viewModel = new UsuariosViewModel();
+    var viewModel = new UsuariosViewModel();
 
-	//insert some fake users for now
-	viewModel.expenses.push(new Expense({
-		desc: "phone call",
-        time: "0.5",
-	}));
+    ////insert some fake users for now
+    //viewModel.expenses.push(new Expense({
+    //	desc: "phone call",
+    //    time: "0.5",
+    //}));
 
-	ko.applyBindings(viewModel);
+    //ko.applyBindings(viewModel);
 });
